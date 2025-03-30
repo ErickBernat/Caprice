7bf689b7929bb715aba5d4199b386bd8d8e0468a
ErickBernat <erickduartebr@gmail.com>
2025-03-30 19:17:23 -0300

    add carrosel marcas

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let listaItens = document.getElementById("listaItens");
-let listaItensDesc = document.getElementById("listaItensDesc");
+let listaItensFiltro = document.getElementById("listaItensFiltro");
 let arrayItens = []
 
 
@@ -22,6 +22,18 @@ function carregaListaItens(){
                     </div>
                 </div>
                 `;
+                listaItensFiltro.innerHTML += `
+                <div class="col mb-4 gap-2 d-flex flex-column align-itens-start">
+                    <div class="card m-0 " style="width: 100%;">
+                        <img src="${item.imagem}" alt="${item.nome}">
+                    </div>
+                    <div class="card-body border-0 d-flex flex-column gap-2">
+                        <h6 class="card-title">${item.nome}</h6>
+                        <p class="card-text">R$ ${item.valor.toFixed(2)}</p>
+                    </div>
+                </div>
+                `;
+                
             });
 
         })
@@ -35,18 +47,33 @@ function carregaListaItensDesconto(){
             let arrayItens = data.itens;
 
             arrayItens.forEach(item => {
-                listaItensDesc.innerHTML += `
-                <div class="col mb-4 gap-2 d-flex flex-column align-itens-start">
-                    <div class="card m-0 " style="width: 100%;">
-                        <img src="${item.imagem}" alt="${item.nome}">
-                    </div>
-                    <div class="card-body border-0 d-flex flex-column gap-2">
-                        <h6 class="card-title">${item.nome}</h6>
-                        <p class="card-text">R$ ${item.valor.toFixed(2)}</p>
-                    </div>
-                </div>
-                `;
+                
             });
 
         })
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector("#carouselExample");
+    const bsCarousel = new bootstrap.Carousel(carousel);
+    let startX = 0;
+    let endX = 0;
+
+    carousel.addEventListener("touchstart", (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchmove", (e) => {
+        endX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchend", () => {
+        let difference = startX - endX;
+
+        if (difference > 50) {
+            bsCarousel.next(); 
+        } else if (difference < -50) {
+            bsCarousel.prev(); 
+        }
+    });
+});
