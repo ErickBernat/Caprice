07280db92c75617c34e731c20449e8b9b4638509
ErickBernat <erickduartebr@gmail.com>
2025-03-31 18:04:13 -0300

    fix responsividade footer

--- script.js
+++ script.js
@@ -77,3 +77,28 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector("#carouselPessoas");
+    const bsCarousel = new bootstrap.Carousel(carousel);
+    let startX = 0;
+    let endX = 0;
+
+    carousel.addEventListener("touchstart", (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchmove", (e) => {
+        endX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchend", () => {
+        let difference = startX - endX;
+
+        if (difference > 50) {
+            bsCarousel.next(); 
+        } else if (difference < -50) {
+            bsCarousel.prev(); 
+        }
+    });
+});
