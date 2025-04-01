667e2de17f60ed668a7cc0fc14841f11f416c400
Erick Duarte Bernat <erickduartebr@gmail.com>
2025-04-01 00:45:31 -0300

    Fix Responsividade Desktop

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function carregaListaItens(){
                     <div class="card m-0 " style="width: 100%;">
                         <img src="${item.imagem}" alt="${item.nome}">
                     </div>
-                    <div class="card-body border-0 d-flex flex-column gap-2">
+                    <div class="card-body texto-card border-0 d-flex flex-column gap-2">
                         <h6 class="card-title">${item.nome}</h6>
                         <p class="card-text">R$ ${item.valor.toFixed(2)}</p>
                     </div>
@@ -27,7 +27,7 @@ function carregaListaItens(){
                     <div class="card m-0 " style="width: 100%;">
                         <img src="${item.imagem}" alt="${item.nome}">
                     </div>
-                    <div class="card-body border-0 d-flex flex-column gap-2">
+                    <div class="card-body texto-card border-0 d-flex flex-column gap-2">
                         <h6 class="card-title">${item.nome}</h6>
                         <p class="card-text">R$ ${item.valor.toFixed(2)}</p>
                     </div>
