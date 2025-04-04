4a12e745fe6d6f790618a7c7818fb127aa460029
ErickBernat <erickduartebr@gmail.com>
2025-04-04 11:48:44 -0300

    Add offCanva carrinho

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function ativaDropDown(botao,drop){
 
 
 
+
 function carregaListaItens(){
 
     fetch('./Data/itens.json')
@@ -52,9 +53,9 @@ function carregaListaItens(){
             arrayItens.forEach(item => {
                 listaItens.innerHTML += `
                 <div class="col mb-4 gap-2 d-flex flex-column align-itens-start">
-                    <div class="card m-0 " style="width: 100%;">
+                    <div class="card m-0 " id="${item.id}" style="width: 100%;">
                         <img src="${item.imagem}" alt="${item.nome}">
-                        <div class="subMenuCard">
+                        <div id="sub${item.id}" class="subMenuCard">
                             <svg xmlns="http://www.w3.org/2000/svg"  class="bi bi-cart-check-fill" viewBox="0 0 16 16">
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-1.646-7.646-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L8 8.293l2.646-2.647a.5.5 0 0 1 .708.708"/>
                             </svg>
@@ -75,6 +76,8 @@ function carregaListaItens(){
                     </div>
                 </div>
                 `;
+
+
                 listaItensFiltro.innerHTML += `
                 <div class="col mb-4 gap-2 d-flex flex-column align-itens-start">
                     <div class="card m-0 " style="width: 100%;">
@@ -89,9 +92,29 @@ function carregaListaItens(){
                 
             });
 
+            arrayItens.forEach(item => {
+                ativaSubMenu(item.id);
+            });
+
         })
 }
 
+function ativaSubMenu(id){
+    let  card = document.getElementById(`${id}`);
+    let submenu = document.getElementById(`sub${id}`)
+    card.addEventListener("mouseenter", () =>{
+        submenu.style.display = 'flex ';
+        submenu.style.animation = 'slideScale 0.4s ease'
+    })
+    card.addEventListener("mouseleave", () =>{
+          submenu.style.animation = 'slideScaleOut  0.8s ease'
+          setTimeout(() => {
+            submenu.style.display = 'none ';
+          }, 400);
+   
+      
+    })
+}
 
         
 
