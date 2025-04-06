5bf2f33aa5affa183557e4c674cb68ac187fcd44
Erick Duarte Bernat <erickduartebr@gmail.com>
2025-04-06 12:50:54 -0300

    Add about finalizada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const listaItens = document.getElementById("listaItens");
 const listaItensFiltro = document.getElementById("listaItensFiltro");
+const listaMembros = document.getElementById("listaMembros");
 const listaModelos = document.getElementById("Carrosel-modelos");
 const html =document.getElementsByTagName('html')
 let arrayItens = []
@@ -109,6 +110,33 @@ function carregaListaItens(){
             arrayItens.forEach(item => {
                 ativaSubMenu(item.id);
             });
+
+        })
+}
+
+function carregaListaMembros(){
+
+    fetch('../Data/membros.json')
+        .then(response => response.json())
+        .then(data => {
+            let arrayItens = data.membros;
+
+            arrayItens.forEach(item => {
+                listaMembros.innerHTML += `
+                <div class="col mb-4 gap-2 d-flex flex-column align-itens-start">
+                    <div class="card m-0 " " style="width: 100%;">
+                        <img src="${item.imagem}" alt="${item.nome}">
+                    <div class="card-body texto-card border-0 d-flex flex-column gap-2">
+                        <h6 class="card-title">${item.nome}</h6>
+                        <p class="card-text">${item.cargo}</p>
+                    </div>
+                    </div>
+                    
+                </div>
+                `;
+            });
+
+            
 
         })
 }
