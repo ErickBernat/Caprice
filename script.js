e98de56f7148f9ee1fb880fc2783341c8e87b2ee
ErickBernat <erickduartebr@gmail.com>
2025-04-07 16:30:55 -0300

    Add blog finalizado

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const listaItens = document.getElementById("listaItens");
 const listaItensFiltro = document.getElementById("listaItensFiltro");
 const listaMembros = document.getElementById("listaMembros");
 const listaModelos = document.getElementById("Carrosel-modelos");
+const listaNoticiasBlog = document.getElementById("containerItensNoticias")
 const html =document.getElementsByTagName('html')
 let arrayItens = []
 let arrayModelos = []
@@ -41,7 +42,57 @@ function ativaDropDown(botao,drop){
 
 
 
+function carregaNoticiasBlog(){
 
+    fetch('../Data/noticias.json')
+        .then(response => response.json())
+        .then(data =>{
+            let arrayNoticias = data.noticias;
+
+            arrayNoticias.forEach(item => {
+                listaNoticiasBlog.innerHTML+=`
+                    <div class="itemNoticia">
+                        <img src="${item.url}" alt="">
+                        <h4>${item.titulo}</h4>
+                        <h6>By by John Snow - April 17,2019</h6>
+                        <p>Ut tempus leo sed magna hendrerit, non congue libero blandit.
+                        Vestibulum libero diam, convallis in arcu ut, semper vulputate
+                        dolor. Vestibulum ante ipsum primis in faucibus orci luctus et 
+                        ultrices posuere cubilia Curae usce luctus fringilla fermentum…</p>
+                        <button>Read More</button>
+                        <div class="bordaEspacamento">
+
+                        </div>
+                    </div>`
+            })
+            listaNoticiasBlog.innerHTML+=`
+             <div class="mensagem">
+                      <p>“Highly customizable. Excellent design. Customer services has exceeded my expectation.
+                          They are quick to answer and even when they don’t know the answer right away.
+                          They’ll work with you towards a solution”</p>
+                        <h6>Michael Franklin</h6>
+                    </div>
+                    `
+
+                    listaNoticiasBlog.innerHTML+=`
+        <div class="itemNoticia">
+                      <img src="../Assets/Images/Modelos/categories-4.jpg" alt="">
+                      <h4>Always Look On The Bright Side Of Life</h4>
+                      <h6>By by John Snow - April 17,2019</h6>
+                      <p>Ut tempus leo sed magna hendrerit, non congue libero blandit.
+                        Vestibulum libero diam, convallis in arcu ut, semper vulputate
+                       dolor. Vestibulum ante ipsum primis in faucibus orci luctus et 
+                       ultrices posuere cubilia Curae usce luctus fringilla fermentum…</p>
+                       <button>Read More</button>
+                       <div class="bordaEspacamento">
+
+                       </div>
+        `
+        })
+        
+
+    
+}
 
 
 function carregaListaItens(){
@@ -135,9 +186,6 @@ function carregaListaMembros(){
                 </div>
                 `;
             });
-
-            
-
         })
 }
 
