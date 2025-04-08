d3e67185b078af18cb89a870d49d3a91610175ac
ErickBernat <erickduartebr@gmail.com>
2025-04-08 16:21:43 -0300

    Add blog modularizado

--- Scripts/blog.js
+++ Scripts/blog.js
@@ -3,5 +3,8 @@ import { carrega }  from "./iniciaComponentesAboutUs.js";
 
 document.addEventListener('DOMContentLoaded',()=>{
     carrega('../Components/header.html', '#header');
+    carrega('../Components/blog.html', '#blog');
+    carrega('../Components/emailInput.html', '#emailInput');
+    carrega('../Components/footer.html','#footer');
     iniciaNoticiasBlog()
 })
